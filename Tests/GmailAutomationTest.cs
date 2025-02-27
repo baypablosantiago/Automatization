fd1db62f8501ed52ef633714e8de3624f24a605c
Pablo Tesoreria <bay.pablo.santiago@gmail.com>
2025-02-27 09:28:31 -0300

    GmailAutomation integration test ready

---
+++ Tests/GmailAutomationTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using Microsoft.Playwright;
+
+public class GmailAutomationTests // INTEGRATION TEST
+{
+    [Fact]
+    public async Task SendMail_ShouldSendExampleMailWithOpenSession()
+    {
+        // Arrange
+        string userDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Google", "Chrome", "User Data");
+        BrowserService browserService = new BrowserService(userDataPath);
+        GmailAutomation gmailAutomation = new GmailAutomation(browserService, "Correo automatizado de ejemplo", "Hello world, este es un correo automatizado de ejemplo.");
+        // Act
+        await gmailAutomation.SendMail();
+        // Assert no hay uno concreto, pero si llega hasta el final sin errores, el test es exitoso.
+    }
+}
--- Tests/YoutubeAutomationTests.cs
+++ Tests/YoutubeAutomationTests.cs
@@ -1,20 +1,16 @@
 using Xunit;
 using Microsoft.Playwright;
 
-
 public class YotubeAutomationTests // INTEGRATION TEST
 {
     [Fact]
     public async Task PlaySong_ShouldOpenTheBrowserAndPlayNeverGiveYouUp()
     {
-        // Arrange: Crear instancia real de Playwright
+        // Arrange
         var browserService = new BrowserService();
         var youtubeAutomation = new YoutubeAutomation(browserService);
-
-        // Act: Ejecutar la automatización
+        // Act
         await youtubeAutomation.PlaySong("Never Give You Up");
-
-        // Assert: No hay una aserción estricta aquí, pero si llega hasta el final sin errores, el test es exitoso.
-
+        // Assert no hay uno concreto, pero si llega hasta el final sin errores, el test es exitoso.
     }
 }
